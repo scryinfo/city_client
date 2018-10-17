042a328e63457a29102084cb53b702e8e27d259c
abel <abel@scry.info>
2018-10-17 16:42:46 +0800

    添加 AddLuaComponent 接口注释

--- Assets/CityGame/Scripts/Framework/CityLuaUtil.cs
+++ Assets/CityGame/Scripts/Framework/CityLuaUtil.cs
@@ -49,6 +49,7 @@ namespace City
             return UIRoot.getPopupRoot();
         }
 
+        // go 是脚本要挂到的目标对象，一般是一个prefab实例； luaPath 是要挂到目标对象上的lua脚本的路径
         public static Component AddLuaComponent(GameObject go, string luaPath)
         {
             LuaComponent com = go.AddComponent<LuaComponent>() as LuaComponent;

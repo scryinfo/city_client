1b139c36a987be43864e07b0e2ea4e43cd97b312
fisher <fisher@scry.info>
2018-10-18 14:29:22 +0800

    //选择仓库

--- Assets/CityGame/Lua/View/ChooseWarehousePanel.lua
+++ Assets/CityGame/Lua/View/ChooseWarehousePanel.lua
@@ -11,9 +11,24 @@ function ChooseWarehousePanel.Awake(obj)
     this.InitPanel();
 end
 function ChooseWarehousePanel.InitPanel()
-    this.returnBtn = transform:Find("ReturnBtn").gameObject;
-    this.searchBtn = transform:Find("SearchBtn").gameObject;
+    this.returnBtn = transform:Find("returnBtn");
+    this.searchBtn = transform:Find("searchBtn");
+    this.arrowBtn = transform:Find("Right/SortingBtn/arrowBtn");
+    this.nowText = transform:Find("Right/SortingBtn/bg/nowText"):GetComponent("Text");
+    this.list = transform:Find("Right/SortingBtn/List").gameObject;
+    this.nameBtn = transform:Find("Right/SortingBtn/List/nameBtn");
+    this.quantityBtn = transform:Find("Right/SortingBtn/List/quantityBtn");
+    this.priceBtn = transform:Find("Right/SortingBtn/List/priceBtn");
+    this.timeBtn = transform:Find("Right/SortingBtn/List/timeBtn");
+    --Mine
+    this.faceImg = transform:Find("Left/mine/FriendsLineItem/face");
+    this.nameText = transform:Find("Left/mine/FriendsLineItem/name");
+    this.boxImg = transform:Find("Left/mine/FriendsLineItem/box").gameObject;
+    --Addresslist  ScrollView
+    this.leftcontent = transform:Find("Left/ScrollView/Viewport/Content");
+    --Right  ScrollView
+    this.rightContent = transform:Find("Right/ScrollView/Viewport/Content");
 end
-function ChooseWarehousePanel.OnDeOnDestroy()
-    destroy(gameObject);
+function ChooseWarehousePanel.OnDestroy()
+    logWarn("OnDestroy ChooseWarehousePanel--->>>");
 end

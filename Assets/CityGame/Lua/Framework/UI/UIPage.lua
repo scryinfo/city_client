dc3c064db657c9d5c98d6d1b176cbaa803dc0a72
abel <abel@scry.info>
2018-10-22 11:22:33 +0800

    修改错误注释

--- Assets/CityGame/Lua/Framework/UI/UIPage.lua
+++ Assets/CityGame/Lua/Framework/UI/UIPage.lua
@@ -370,7 +370,7 @@ function UIPage:ClosePageByName(pageName)
     end
 end
 
---注册每个 Controller 的打开方法，因为打开方法是在该Controller实例化之前，所以，目前暂时放到一个全局函数中， 在每个 Controller 类声明后调用就行
+--注册每个 Controller 的打开方法，因为打开方法是在该Controller实例化之前，所以， 在每个 Controller 类声明后调用就行
 function UIPage:ResgisterOpen(inClass)
     Event.AddListener('c_OnOpen'..inClass.name, function (data)
         UIPage:ShowPage(inClass,data)

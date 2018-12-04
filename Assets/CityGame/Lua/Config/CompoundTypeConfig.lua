7643e089beba8eb5a07b29feeab3ab5d39987482
cycle <cycle@scry.info>
2018-12-04 18:27:28 +0800

    //添加线-合成界面

---
+++ Assets/CityGame/Lua/Config/CompoundTypeConfig.lua
@@ -0,0 +1,143 @@
+CompoundTypeConfig={
+
+	[2151] = {
+		
+		[1] = {
+		itemId = 2151001,
+		name = "fooddata", 
+		itemType = 0,
+		matCompoundGoods = {2201001,2201003,2201004}, 
+		goodsNeedMatData = {},
+		},
+		
+		[2] = {
+		itemId = 2151002,
+		name = "fooddata", 
+		itemType = 0,
+		matCompoundGoods = {2201002,2201004}, 
+		goodsNeedMatData = {},
+		},
+		
+		[3] = {
+		itemId = 2151003,
+		name = "fooddata", 
+		itemType = 0,
+		matCompoundGoods = {2201003,2201004}, 
+		goodsNeedMatData = {},
+		},
+	},
+	
+	
+	
+	[2152] = {
+		
+		[1] = {
+		itemId = 2152001,
+		name = "dressdata", 
+		itemType = 0,
+		matCompoundGoods = {2202001,2202003,2202004}, 
+		goodsNeedMatData = {},
+		},
+		
+		[2] = {
+		itemId = 2152002,
+		name = "dressdata", 
+		itemType = 0,
+		matCompoundGoods = {2202002,2202004}, 
+		goodsNeedMatData = {},
+		},
+		
+		[3] = {
+		itemId = 2152003,
+		name = "dressdata", 
+		itemType = 0,
+		matCompoundGoods = {2202003,2202004}, 
+		goodsNeedMatData = {},
+		},
+	},
+	
+	[2201] = {
+		[1] = {
+		itemId = 2201001,
+		name = "food",
+		itemType = 1,
+		matCompoundGoods = {},
+		goodsNeedMatData = {
+			[1] = {itemId = 2151001, num = 1},
+		}
+		},
+		[2] = {
+		itemId = 2201002,
+		name = "food",
+		itemType = 1,
+		matCompoundGoods = {},
+		goodsNeedMatData = {
+			[1] = {itemId = 2151002, num = 1},
+		}
+		},
+		[3] = {
+		itemId = 2201003,
+		name = "food",
+		itemType = 1,
+		matCompoundGoods = {},
+		goodsNeedMatData = {
+			[1] = {itemId = 2151001, num = 1},
+			[2] = {itemId = 2151003, num = 1},
+		}
+		},
+		[4] = {
+		itemId = 2201004,
+		name = "food",
+		itemType = 1,
+		matCompoundGoods = {},
+		goodsNeedMatData = {
+			[1] = {itemId = 2151001, num = 1},
+			[2] = {itemId = 2151002, num = 1},
+			[3] = {itemId = 2151003, num = 1},
+		}
+		}
+	}	
+
+	
+	[2202] = {
+		[1] = {
+		itemId = 2202001,
+		name = "dress",
+		itemType = 1,
+		matCompoundGoods = {},
+		goodsNeedMatData = {
+			[1] = {itemId = 2152001, num = 1},
+		}
+		},
+		[2] = {
+		itemId = 2202002,
+		name = "dress",
+		itemType = 1,
+		matCompoundGoods = {},
+		goodsNeedMatData = {
+			[1] = {itemId = 2152002, num = 1},
+		}
+		},
+		[3] = {
+		itemId = 2202003,
+		name = "dress",
+		itemType = 1,
+		matCompoundGoods = {},
+		goodsNeedMatData = {
+			[1] = {itemId = 2152001, num = 1},
+			[2] = {itemId = 2152003, num = 1},
+		}
+		},
+		[4] = {
+		itemId = 2202004,
+		name = "dress",
+		itemType = 1,
+		matCompoundGoods = {},
+		goodsNeedMatData = {
+			[1] = {itemId = 2152001, num = 1},
+			[2] = {itemId = 2152002, num = 1},
+			[3] = {itemId = 2152003, num = 1},
+		}
+		}
+	}
+}

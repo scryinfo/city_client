317ee0169c78c8ed29abc3bfd69caca02b96e4c1
dy <123456>
2019-01-05 10:07:04 +0800

    设置工资

--- Assets/CityGame/Lua/Controller/WagesAdjustBoxCtrl.lua
+++ Assets/CityGame/Lua/Controller/WagesAdjustBoxCtrl.lua
@@ -85,7 +85,7 @@ function WagesAdjustBoxCtrl:_onClickConfim(ins)
     data.type="begin"
     data.mainText="Processing plant successfully opened"
     data.callback=function()
-                     Event.Brocast("m_ReqHouseSetSalary1",m_data.buildInfo.id,tonumber(inputValue))
+                     Event.Brocast("m_ReqHouseSetSalary1",m_data.buildInfo.id,100)
                      Event.Brocast("m_startBusiness",m_data.buildInfo.id)
                      Event.Brocast("mCloes")
                      --m_data:func()

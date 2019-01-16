c34b4067792aa5b1a4dd60965fd0d7bbfe877fbb
abel <abel@scry.info>
2019-01-17 03:09:09 +0800

    提交

--- build.bat
+++ build.bat
@@ -1,7 +1,19 @@
 @echo off  
+
+echo start gen wrap files.............  
+
+D:\Unity\Editor\Unity.exe -quit -batchmode -executeMethod CityBuild._genWrapFiles -logFile build.log
  
+echo gen wrap files finished.............!  
+
 echo start apk building.............  
  
-D:\Unity\Editor\Unity.exe -batchmode -executeMethod CityBuild.Build -logFile build.log
+D:\Unity\Editor\Unity.exe -quit -batchmode -executeMethod CityBuild.Build -logFile build.log
  
-echo APK building finished.............!  
+echo apk building finished.............!  
+
+echo start res building.............  
+ 
+D:\Unity\Editor\Unity.exe -quit -batchmode -executeMethod CityBuild.BuildResourceBundle -logFile build.log
+ 
+echo res building finished.............!  

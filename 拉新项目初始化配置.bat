3647822c35d5729b3b83573bf1f229ef86c7f4d7
abel <abel@scry.info>
2019-01-17 00:33:10 +0800

    1

--- 拉新项目初始化配置.bat
+++ 拉新项目初始化配置.bat
@@ -1,6 +1,10 @@
 @echo off
 
 @如果从git拉新项目，无法编译，执行本文件即可
+if exist Assets\CityGame\Source\Generate (
+	rd/s/q Assets\CityGame\Source\Generate
+)
+md Assets\CityGame\Source\Generate
 XCOPY Source_Generate Assets\CityGame\Source\Generate /s/e/y
 
 echo "copying files finished"

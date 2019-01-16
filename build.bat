c97712d515e9c390ceedf6cb1c33f7c8809e5e8e
abel <abel@scry.info>
2019-01-16 13:00:25 +0800

    1

--- build.bat
+++ build.bat
@@ -2,7 +2,6 @@
  
 echo start apk building.............  
  
-D:\Unity\Editor\Unity.exe -quit -batchmode -executeMethod CityBuild.Build -logFile build.log
+D:\Software\Unity\Editor\Unity.exe -quit -batchmode -executeMethod CityBuild.Build -logFile build.log
  
 echo APK building success.............!  
-pause 

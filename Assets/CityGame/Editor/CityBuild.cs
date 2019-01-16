f1c58838a8f3f3135574ed58b3cca3311ad6b4e1
abel <abel@scry.info>
2019-01-17 00:32:33 +0800

    1

--- Assets/CityGame/Editor/CityBuild.cs
+++ Assets/CityGame/Editor/CityBuild.cs
@@ -7,6 +7,11 @@ using LuaFramework;
 
 public class CityBuild : Editor
 {
+    static void _BuildAndroidResource()
+    {
+        Packager.BuildAndroidResource();
+        AssetDatabase.Refresh();
+    }
 
     [MenuItem("Tool/APKBuild")]
     public static void Build()
@@ -37,12 +42,27 @@ public class CityBuild : Editor
         // 打包出 APK 名
         string apkName = string.Format("./{0}.apk", "Test");
         // 执行打包
-        string res = BuildPipeline.BuildPlayer(levels.ToArray(), apkName, buildTarget, BuildOptions.None);
-        Debug.Log("apk build successfully, file location = "+res);
+        //string res = BuildPipeline.BuildPlayer(levels.ToArray(), apkName, buildTarget, BuildOptions.None);
+        //Debug.Log("apk build successfully, file location = "+res);
+
+        System.Threading.Thread.Sleep(5);
+
+        ToLuaMenu.GenLuaWrapBinder();
         AssetDatabase.Refresh();
+
         AppFacade.createGameManager();
+        AppFacade.Instance.AddManager<PanelManager>(ManagerName.Panel);
+        AppFacade.Instance.AddManager<SoundManager>(ManagerName.Sound);
+        AppFacade.Instance.AddManager<BuildManager>(ManagerName.Build);
+        AppFacade.Instance.AddManager<RayManager>(ManagerName.Ray);
+        AppFacade.Instance.AddManager<TimerManager>(ManagerName.Timer);
+        ResourceManager resmgr = AppFacade.Instance.AddManager<ResourceManager>(ManagerName.Resource);
+        AppFacade.Instance.AddManager<ThreadManager>(ManagerName.Thread);
+        AppFacade.Instance.AddManager<ObjectPoolManager>(ManagerName.ObjectPool);
+        GameManager gmgr = AppFacade.Instance.GetManager<GameManager>(ManagerName.Game);
         AppFacade.Instance.AddManager<GameManager>(ManagerName.Game);
-        GameManager gmgr = AppFacade.Instance.GetManager<GameManager>(ManagerName.Game);        
-        Packager.BuildAndroidResource();
+        LuaManager luemgr = AppFacade.Instance.AddManager<LuaManager>(ManagerName.Lua);
+
+        _BuildAndroidResource();
     }
 }

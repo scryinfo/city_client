476e61ea80b45d742f70a9d773b37b4edfdee18d
scryabel <42855048+scryabel@users.noreply.github.com>
2019-01-17 17:38:40 +0800

    1

--- 拉新项目初始化配置.bat
+++ 拉新项目初始化配置.bat
@@ -4,7 +4,6 @@
 if exist Assets\CityGame\Source\Generate (
 	rd/s/q Assets\CityGame\Source\Generate
 )
-
 md Assets\CityGame\Source\Generate
 XCOPY Source_Generate Assets\CityGame\Source\Generate /s/e/y/f
 

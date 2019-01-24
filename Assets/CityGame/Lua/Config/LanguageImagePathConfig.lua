9fa01abf8b399864a6a53ec1313144936e039d53
dy <123456>
2019-01-24 20:37:41 +0800

    11

--- Assets/CityGame/Lua/Config/LanguageImagePathConfig.lua
+++ Assets/CityGame/Lua/Config/LanguageImagePathConfig.lua
@@ -3,18 +3,19 @@
 --- Created by xuyafang.
 --- DateTime: 2019/1/24 17:04
 ---
-LanguageImagePathConfig =
+Sprite_Chinese =
 {
     --建议命名：英文以LE_开头，中文以LC_开头，其他不变，这样就可以直接复制粘贴统一修改了
-    ["EN"] =
-    {
-        ["UIBubbleSell"] = "Assets/CityGame/Resources/Atlas/multilanguage/LE_bubble-sell.png",
-        ["UIBubbleRent"] = "Assets/CityGame/Resources/Atlas/multilanguage/LE_bubble-rent.png",
-    },
+    ["UIBubbleSell"] = "Assets/CityGame/Resources/Atlas/multilanguage/LE_bubble-sell.png",
+    ["UIBubbleRent"] = "Assets/CityGame/Resources/Atlas/multilanguage/LE_bubble-rent.png",
 
-    ["CN"] =
-    {
-        ["UIBubbleSell"] = "Assets/CityGame/Resources/Atlas/multilanguage/LC_bubble-sell.png",
-        ["UIBubbleRent"] = "Assets/CityGame/Resources/Atlas/multilanguage/LC_bubble-rent.png",
-    }
+
+
+}
+
+Sprite_English={
+
+   ["UIBubbleSell"] = "Assets/CityGame/Resources/Atlas/multilanguage/LC_bubble-sell.png",
+   ["UIBubbleRent"] = "Assets/CityGame/Resources/Atlas/multilanguage/LC_bubble-rent.png",
+
 }

41cefb651adb9f32fcc7b2b2a619b40ed001a3c6
abel <abel@scry.info>
2019-01-25 21:25:53 +0800

    1

--- Assets/CityGame/Scripts/ConstDefine/AppConst.cs
+++ Assets/CityGame/Scripts/ConstDefine/AppConst.cs
@@ -38,8 +38,8 @@ namespace LuaFramework {
         public const string AssetDir = "StreamingAssets";           //素材目录 
         //public const string WebUrl = "http://localhost:6688/";      //测试更新地址
         //public const string WebUrl = "http://192.168.0.188:8080/CityHotUp/";      //测试更新地址
-        public const string WebUrl = "http://192.168.0.191:8080/CityHotUp/";      //测试更新地址
-        //public const string WebUrl = "http://40.73.5.184:8080/city/";      //测试更新地址
+        //public const string WebUrl = "http://192.168.0.191:8080/CityHotUp/";      //测试更新地址
+        public const string WebUrl = "http://40.73.5.184:8080/city/";      //测试更新地址
         public const string AssetDir_CloseBundleMode = "View";        //关闭资源 BundleMode 后的资源读取路径
         public static string FrameworkRoot {
             get {

58cc73f1748da9778cbe734d55bf0ba5da614aa9
abel <abel@scry.info>
2019-01-26 16:40:14 +0800

    添加

---
+++ UploadResource.bat
@@ -0,0 +1,33 @@
+echo off
+
+::工具目录
+
+set scptool=D:\tools\winscp\
+
+set local_dir=..\client\Assets\StreamingAssets\
+
+set remote_dir= /home/cityrs/city1
+
+::账号
+
+set account=root
+
+set password=djf0KJop9823u7j*
+
+set host=40.73.5.184
+
+set port=22
+
+::切换到WinSCP软件目录
+
+D:
+
+cd %scptool%
+
+::调用软件 执行上传
+
+WinSCP.exe /console /command "option batch continue" "option confirm off" "open root:djf0KJop9823u7j*@40.73.5.184:22" "cd /home/cityrs/city1" "lcd ..\client\Assets\StreamingAssets\" "option transfer binary" "put ..\client\Assets\StreamingAssets\*.* /home/cityrs/city " "close" "exit"
+
+echo upload completed.....
+
+pause

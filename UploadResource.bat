a9cab53400ce44278b867f7a2f6cb716a15fc920
abel <abel@scry.info>
2019-01-26 16:31:50 +0800

    1

--- UploadResource.bat
+++ UploadResource.bat
@@ -4,13 +4,13 @@ echo off
 
 set scptool=D:\tools\winscp\
 
-set local_dir=..\client\Assets\StreamingAssets
+set local_dir=..\client\Assets\StreamingAssets\
 
 set remote_dir= /home/cityrs/city1
 
 ::账号
 
-set account=city
+set account=root
 
 set password=djf0KJop9823u7j*
 
@@ -26,7 +26,7 @@ cd %scptool%
 
 ::调用软件 执行上传
 
-call WinSCP.exe /console /command "option batch continue" "option confirm off" "open %account%:%password%@%host%:%port%" "cd %remote_dir%" "lcd %local_dir%" "option transfer binary" "put -filemask=|*git/;*svn/;*META-INF/;*WEB-INF/;*csv/;*idea/;*DS_Store/;*hg/;*.bat;*.gitignore;*.iml;*settings/;*project/;*.hprof;*.pyc;*.bat %local_dir%*.* %remote_dir% " "close" "exit"
+WinSCP.exe /console /command "option batch continue" "option confirm off" "open root:djf0KJop9823u7j*@40.73.5.184:22" "cd /home/cityrs/city1" "lcd G:\city_project\client\Assets\StreamingAssets\" "option transfer binary" "put G:\city_project\client\Assets\StreamingAssets\*.* /home/cityrs/city1 " "close" "exit"
 
 echo upload completed.....
 

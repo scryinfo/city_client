dbd27ee40c422a4d062790d29b033b80af77e24e
abel <abel@scry.info>
2019-01-26 17:03:33 +0800

    1

--- build.bat
+++ build.bat
@@ -18,25 +18,7 @@ D:\Unity\Editor\Unity.exe -quit -batchmode -executeMethod CityBuild.BuildResourc
 
 echo res building finished  at  %time%.............!  
 
-
-echo start update resources to hot update directory   at  %time%.............  
-
-echo check exist of directory D:\hotUp\CityHotUp
-if exist D:\hotUp\CityHotUp (
-	echo delete old directory of D:\hotUp\CityHotUp
-	rd/s/q D:\hotUp\CityHotUp
-)
-
-echo create new directory of D:\hotUp\CityHotUp
-md D:\hotUp\CityHotUp
-
-echo copy new resources from StreamingAssets to D:\hotUp\CityHotUp
-XCOPY ..\client\Assets\StreamingAssets  D:\hotUp\CityHotUp /s /e /k /h /g /c /r/y
-
-echo copy new Apk to D:\city_project\apk
+echo copy new Apk to D:\city_project\apk  
 XCOPY ..\client\Apk  D:\city_project\apk /s /e /k /h /g /c /r/y
-
-echo delete Apk in source directory ..\client\Apk
 rd/s/q ..\client\Apk
 
-echo hot update resources updating finished at  %time%.............  

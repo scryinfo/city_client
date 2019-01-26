902396940ccec3a121f190a0becbf6f85c0f026f
abel <abel@scry.info>
2019-01-26 15:31:39 +0800

    提交

--- build.bat
+++ build.bat
@@ -24,6 +24,7 @@ echo start update resources to hot update directory   at  %time%.............
 if exist D:\hotUp\CityHotUp (
 	rd/s/q D:\hotUp\CityHotUp
 )
+
 md D:\hotUp\CityHotUp
 
 XCOPY ..\client\Assets\StreamingAssets  D:\hotUp\CityHotUp /s /e /k /h /g /c /r/y

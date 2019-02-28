09767e7433e6ab56893b92703bf6bc075df71e48
yangcaomin <810234671@qq.com>
2019-02-28 18:36:58 +0800

    121

--- Assets/CityGame/Lua/Game.lua
+++ Assets/CityGame/Lua/Game.lua
@@ -43,7 +43,7 @@ function Game.OnInitOK()
     --初始化屏幕适配比例--
     InitScreenRatio()
     
-    ct.OpenCtrl('LoginCtrl')
+    ct.OpenCtrl('LoadingCtrl')
     --ct.OpenCtrl('LoginCtrl',Vector2.New(0, 0)) --注意传入的是类名
 end
 

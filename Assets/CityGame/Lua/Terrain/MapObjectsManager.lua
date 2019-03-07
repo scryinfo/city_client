f9ef5477d9f728afbefe74134385d5d1d138d095
yangcaomin <810234671@qq.com>
2019-03-07 11:34:08 +0800

    遍历保存所有材质球

--- Assets/CityGame/Lua/Terrain/MapObjectsManager.lua
+++ Assets/CityGame/Lua/Terrain/MapObjectsManager.lua
@@ -1,14 +1,43 @@
 MapObjectsManager = {}
+local AllMaterial = {}
 local AllObjectPools = {}
+local RendererType = nil
 
 --创建Prefab成功后初始对应Pool
 local function CreateBasePrefabSuccess(tempPrefab,item)
-    AllObjectPools[item.Name] = LuaGameObjectPool:new(item.Name,tempPrefab,item.InitCount,MapGameObjectsConfig.HidePosition)
+    if tempPrefab ~= nil then
+        --初始化对象池
+        AllObjectPools[item.Name] = LuaGameObjectPool:new(item.Name,tempPrefab,item.InitCount,MapGameObjectsConfig.HidePosition)
+    end
 end
+
+function MapObjectsManager.AddMaterial(ParentObj)
+    collectgarbage("collect")
+    local c1 = collectgarbage("count")
+    if RendererType ~= nil then
+        local allrender = ParentObj:GetComponentsInChildren(RendererType):ToTable()
+        if allrender ~= nil then
+            for i, tempRender in pairs(allrender) do
+                for i, tempMaterial in pairs(tempRender.materials:ToTable()) do
+                    local tempInsId = tempMaterial:GetInstanceID()
+                    if  AllMaterial[tempInsId] == nil then
+                        AllMaterial[tempInsId] = tempMaterial
+                    end
+                end
+            end
+        end
+    end
+    local c2 = collectgarbage("count")
+    ct.log("system",   c2 -c1)
+    collectgarbage("collect")
+end
+
 
 --初始化所有的建筑对象池
 function MapObjectsManager.Init()
+    AllMaterial = {}
     AllObjectPools = {}
+    RendererType = typeof(UnityEngine.Renderer)
     --初始化基础建筑Prefeb（异步）
     local PoolInstantiates = MapGameObjectsConfig.PoolInstantiate
     for i, item in pairs(PoolInstantiates) do
@@ -34,5 +63,16 @@ end
 function MapObjectsManager.RecyclingGameObjectToPool(poolName,go)
     if AllObjectPools[poolName] ~= nil and go ~= nil then
         AllObjectPools[poolName]:RecyclingGameObjectToPool(go)
+    end
+end
+
+
+function MapObjectsManager.ChangeShader()
+    local tempColor = getColorByInt(math.random(0, 255),math.random(0, 255),math.random(0, 255),1)
+    for i, tempMaterial in pairs(AllMaterial) do
+        tempMaterial:SetColor("_DiffuseColor",tempColor)
+        tempMaterial:SetColor("_DayLightColor",tempColor)
+        tempMaterial:SetFloat("_DayLightStrength",1)
+        tempMaterial:SetFloat("_LightStrengh",1)
     end
 end

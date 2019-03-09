c963929a5af889de696c3b4bb3833c7926074bb2
fisher <fisher@scry.info>
2019-03-09 17:26:08 +0800

    //

--- Assets/CityGame/Lua/View/BuildingInfo/ShelfGoodsItem.lua
+++ Assets/CityGame/Lua/View/BuildingInfo/ShelfGoodsItem.lua
@@ -4,28 +4,32 @@ ShelfGoodsItem = class('ShelfGoodsItem')
 function ShelfGoodsItem:initialize(goodsDataInfo,prefab,inluabehaviour,id,info)
     self.id = id
     self.state = info[1]
-    self.prefab = prefab;
+    self.prefab = prefab
     self.buildingId = info[2]
-    self.goodsDataInfo = goodsDataInfo;
+    self.goodsDataInfo = goodsDataInfo
     self._luabehaviour = inluabehaviour
     self.num = goodsDataInfo.n
     self.itemId = goodsDataInfo.k.id
     self.price = goodsDataInfo.price
-    self.bgBtn = self.prefab.transform:Find("bgBtn");  --物品btn，点击勾选物品，默认为false
-    self.shelfImg = self.prefab.transform:Find("shelfImg").gameObject;  --架子
-    self.goodsicon = self.prefab.transform:Find("details/goodsicon"):GetComponent("Image");  --物品Icon
-    self.circleGreayImg = self.prefab.transform:Find("circleGreayImg"):GetComponent("RectTransform");  --圆
-    self.circleTickImg = self.prefab.transform:Find("circleTickImg"):GetComponent("RectTransform");  --勾选
-    self.nameText = self.prefab.transform:Find("details/nameText"):GetComponent("Text");  --物品名字
-    self.numberText = self.prefab.transform:Find("details/numberText"):GetComponent("Text");  --物品数量
-    self.moneyText = self.prefab.transform:Find("moneyImg/moneyText"):GetComponent("Text");  --物品价格
-    self.XBtn = self.prefab.transform:Find("XBtn"):GetComponent("RectTransform");  --删除按钮
-    self.detailsBtn = self.prefab.transform:Find("detailsBtn");  --点击商品查看详情
+    self.bgBtn = self.prefab.transform:Find("bgBtn")  --物品btn，点击勾选物品，默认为false
+    self.shelfImg = self.prefab.transform:Find("shelfImg").gameObject  --架子
+    self.goodsicon = self.prefab.transform:Find("details/goodsicon"):GetComponent("Image")  --物品Icon
+    self.circleGreayImg = self.prefab.transform:Find("circleGreayImg"):GetComponent("RectTransform")  --圆
+    self.circleTickImg = self.prefab.transform:Find("circleTickImg"):GetComponent("RectTransform")  --勾选
+    self.nameText = self.prefab.transform:Find("details/nameText"):GetComponent("Text")  --物品名字
+    self.numberText = self.prefab.transform:Find("details/numberText"):GetComponent("Text")  --物品数量
+    self.moneyText = self.prefab.transform:Find("moneyImg/moneyText"):GetComponent("Text")  --物品价格
+    self.XBtn = self.prefab.transform:Find("XBtn"):GetComponent("RectTransform")  --删除按钮
+    self.detailsBtn = self.prefab.transform:Find("detailsBtn")  --点击商品查看详情
+    self.materialbg = self.prefab.transform:Find("materialbg")  --原料bg
+    self.goodsbg = self.prefab.transform:Find("goodsbg")  --商品bg
 
     local materialKey,goodsKey = 21,22
     local type = ct.getType(UnityEngine.Sprite)
     if math.floor(self.itemId / 100000) == materialKey then
         --self.nameText.text = Material[self.itemId].name;
+        self.materialbg.transform.localScale = Vector3.one
+        self.goodsbg.transform.localScale = Vector3.zero
         self.nameText.text = GetLanguage(self.itemId);
         panelMgr:LoadPrefab_A(Material[self.itemId].img,type,nil,function(goodData,obj)
             if obj ~= nil then
@@ -34,6 +38,8 @@ function ShelfGoodsItem:initialize(goodsDataInfo,prefab,inluabehaviour,id,info)
             end
         end)
     elseif math.floor(self.itemId / 100000) == goodsKey then
+        self.materialbg.transform.localScale = Vector3.zero
+        self.goodsbg.transform.localScale = Vector3.one
         self.nameText.text = GetLanguage(self.itemId);
         panelMgr:LoadPrefab_A(Good[self.itemId].img,type,nil,function(goodData,obj)
             if obj ~= nil then

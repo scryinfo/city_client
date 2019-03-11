b20c4a57481f54231b75b4576bef6d58e4147bf3
Lily9610 <1015480725@qq.com>
2019-03-11 17:04:08 +0800

    好友红点显示方式修改

--- Assets/CityGame/Lua/Controller/GameMainInterfaceCtrl.lua
+++ Assets/CityGame/Lua/Controller/GameMainInterfaceCtrl.lua
@@ -586,7 +586,11 @@ end
 --好友红点--
 function GameMainInterfaceCtrl._showFriendsNotice()
     local friendsApply = DataManager.GetMyFriendsApply()
-    GameMainInterfacePanel.friendsNotice:SetActive(#friendsApply > 0)
+    if #friendsApply > 0 then
+        GameMainInterfacePanel.friendsNotice.localScale = Vector3.one
+    else
+        GameMainInterfacePanel.friendsNotice.localScale = Vector3.zero
+    end
 end
 
 function GameMainInterfaceCtrl:c_OnReceiveAddFriendReq()

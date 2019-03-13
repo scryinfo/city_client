65899e459c28d381fffbc5fedc5a413be4121340
yangcaomin <810234671@qq.com>
2019-03-13 17:48:24 +0800

    天气系统_天气粒子特效接入

--- Assets/CityGame/Lua/Terrain/ClimateManager.lua
+++ Assets/CityGame/Lua/Terrain/ClimateManager.lua
@@ -5,11 +5,19 @@ local m_Hour = -1
 local m_WeatherID = nil
 local m_DayAndNightID = nil
 
+local m_Rain = nil
+local m_Snow = nil
+
 
 local function ChangeWeather()
-    local WeatherSetting = DayAndNightConfig[m_DayAndNightID]
-    if WeatherSetting ~= nil then
+    m_Rain:SetActive(false)
+    m_Snow:SetActive(false)
+    if m_WeatherID == 0 then        --晴天/阴天
 
+    elseif m_WeatherID == 1 then    --下雨
+        m_Rain:SetActive(true)
+    elseif m_WeatherID == 2 then    --下雪
+        m_Snow:SetActive(true)
     end
 end
 
@@ -25,7 +33,7 @@ local function JudgeChangeWeatherOrChangeDaN(newClimateID)
     local tempWeatherID = math.floor(newClimateID / 100)
     local tempDayAndNightID = newClimateID % 100
     if m_WeatherID == nil or tempWeatherID ~= m_WeatherID then
-        tempWeatherID = m_WeatherID
+        m_WeatherID = tempWeatherID
         ChangeWeather()
     end
     if m_DayAndNightID == nil or m_DayAndNightID ~= tempDayAndNightID then
@@ -56,6 +64,9 @@ function ClimateManager.Init()
     m_Timer = Timer.New(RefreshWeather, 20, -1, true)
     m_Timer.time = 0
     m_Timer:Start()
+
+    m_Rain = UnityEngine.GameObject.Find("CameraRoot/Effect/Effect_Rain")
+    m_Snow = UnityEngine.GameObject.Find("CameraRoot/Effect/Effect_Snow")
 end
 
 

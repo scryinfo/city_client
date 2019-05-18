dc4a1e327573352a3a253c8e36828409f40e9ddd
fisher <fisher@scry.info>
2019-05-18 17:45:13 +0800

    //

--- Assets/CityGame/Lua/Config/BuildingInformationIcon.lua
+++ Assets/CityGame/Lua/Config/BuildingInformationIcon.lua
@@ -2,72 +2,72 @@ BuildingInformationIcon =
 {
     --原料厂
     [1100001] = {
-        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/MaterialBuilding_1x1"
+        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/MaterialBuilding_1x1.png"
     },
     [1100002] = {
-        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/MaterialBuilding_2x2"
+        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/MaterialBuilding_2x2.png"
     },
     [1100003] = {
-        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/MaterialBuilding_3x3"
+        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/MaterialBuilding_3x3.png"
     },
     --加工厂
     [1200001] = {
-        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/Factory_1x1"
+        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/Factory_1x1.png"
     },
     [1200002] = {
-        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/Factory_2x2"
+        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/Factory_2x2.png"
     },
     [1200003] = {
-        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/Factory_3x3"
+        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/Factory_3x3.png"
     },
     --零售店
     [1300001] = {
-        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/SuperMarket_1x1"
+        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/SuperMarket_1x1.png"
     },
     [1300002] = {
-        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/SuperMarket_2x2"
+        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/SuperMarket_2x2.png"
     },
     [1300003] = {
-        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/SuperMarket_3x3"
+        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/SuperMarket_3x3.png"
     },
     --住宅
     [1400001] = {
-        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/HomeHouse_1x1"
+        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/HomeHouse_1x1.png"
     },
     [1400002] = {
-        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/HomeHouse_2x2"
+        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/HomeHouse_2x2.png"
     },
     [1400003] = {
-        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/HomeHouse_3X3"
+        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/HomeHouse_3X3.png"
     },
     --研究所
     [1500001] = {
-        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/Technology_1x1"
+        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/Technology_1x1.png"
     },
     [1500002] = {
-        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/Technology_2x2"
+        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/Technology_2x2.png"
     },
     [1500003] = {
-        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/Technology_3x3"
+        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/Technology_3x3.png"
     },
     --广告公司(推广公司)
     [1600001] = {
-        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/Advertising_1x1"
+        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/Advertising_1x1.png"
     },
     [1600002] = {
-        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/Advertising_2x2"
+        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/Advertising_2x2.png"
     },
     [1600003] = {
-        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/Advertising_3x3"
+        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/Advertising_3x3.png"
     },
     --集散中心
     [1700001] = {
-        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/WareHouse_1x1"
+        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/WareHouse_1x1.png"
     },
     [1700002] = {
-        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/WareHouse_2x2"
+        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/WareHouse_2x2.png"
     },
     [1700003] = {
-        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/WareHouse_3x3"
+        ["imgPath"] ="Assets/CityGame/Resources/Atlas/BuildingInformation/BuildingIcon/WareHouse_3x3.png"
     }
 }

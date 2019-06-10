1baa6e775864649a0ace22f2fd00dc40e7478d57
cycle <cycle@scry.info>
2019-06-10 18:30:12 +0800

    //temp

--- Assets/CityGame/Lua/Items/FlightForecastItems/FlightMainIPagetem.lua
+++ Assets/CityGame/Lua/Items/FlightForecastItems/FlightMainIPagetem.lua
@@ -11,12 +11,15 @@ function FlightMainPageItem:initialize(viewRect, data)
     self.viewRect = viewRect.transform
 
     self.btn = self.viewRect:Find("btn"):GetComponent("Button")
-    self.rateText = self.viewRect:Find("rateText"):GetComponent("Text")  --准点率
-    self.numText = self.viewRect:Find("rateText/numText"):GetComponent("Text")  --航班号
+    --self.rateText = self.viewRect:Find("rateText"):GetComponent("Text")  --准点率
+    self.flightText = self.viewRect:Find("flightText"):GetComponent("Text")  --航班名称
+    self.numText = self.viewRect:Find("timeText/numText"):GetComponent("Text")  --航班号
     self.betMoneyText = self.viewRect:Find("betMoney"):GetComponent("Text")  --累积投注
     self.timeText = self.viewRect:Find("timeText"):GetComponent("Text")  --航班时间
     self.startPlaceText = self.viewRect:Find("startPlaceText"):GetComponent("Text")  --起始位置
     self.endPlaceText = self.viewRect:Find("endPlaceText"):GetComponent("Text")  --目的地
+    self.startCodeText = self.viewRect:Find("startCodeText"):GetComponent("Text")  --起始位置code
+    self.endCodeText = self.viewRect:Find("endCodeText"):GetComponent("Text")  --目的地code
     self.alreadyTran = self.viewRect:Find("alreadyRoot")  --已投注的标识
 
     self.betMoneyText01 = self.viewRect:Find("betMoney/Text"):GetComponent("Text")  --累积押注
@@ -30,8 +33,8 @@ function FlightMainPageItem:initialize(viewRect, data)
 end
 --刷新数据
 function FlightMainPageItem:initData(data)
-    local trueWidth01 = self.rateText.preferredWidth
-    self.rateText.rectTransform.sizeDelta = Vector2.New(trueWidth01, self.rateText.rectTransform.sizeDelta.y)
+    local trueWidth01 = self.timeText.preferredWidth
+    self.timeText.rectTransform.sizeDelta = Vector2.New(trueWidth01, self.timeText.rectTransform.sizeDelta.y)
 
     self.betMoneyText.text = data
     local trueWidth02 = self.betMoneyText.preferredWidth
@@ -39,8 +42,8 @@ function FlightMainPageItem:initData(data)
 end
 --
 function FlightMainPageItem:Language()
-    self.betMoneyText01.text = "累积投注："
-    self.alreadyTranText02.text = "已下注"
+    self.betMoneyText01.text = GetLanguage(32030005)
+    self.alreadyTranText02.text = GetLanguage(32030029)
 end
 --
 function FlightMainPageItem:ClickFunc()

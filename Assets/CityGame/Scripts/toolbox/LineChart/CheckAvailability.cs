bd18967296f8ac8f086d259c9364222626cc13f7
rodger <rodger@scry.info>
2019-07-04 14:53:24 +0800

    检查是否存在CashBox

---
+++ Assets/CityGame/Scripts/toolbox/LineChart/CheckAvailability.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using City;
+using UnityEngine;
+
+public class CheckAvailability : MonoBehaviour
+{
+    //检查钱包是否可用
+    public void OnActivityResult(string resultData)
+    {
+        if (resultData == "NoCashBox")
+        {
+            CityLuaUtil.CallMethod("CityEngineLua", "NoCashBox");
+        }
+        else if (resultData == "Fail")
+        {
+            CityLuaUtil.CallMethod("CityEngineLua", "Fail");
+        }
+        else if (resultData == "Success")
+        {
+            CityLuaUtil.CallMethod("CityEngineLua", "GotoCashBox");
+        }     
+    }
+
+}

91230cee543886ecfabd4afd1eea4eb6c0f07986
fisher <fisher@scry.info>
2019-07-17 18:11:29 +0800

    //仓库刷新，上架详情数量刷新

--- Assets/CityGame/Lua/View/BuildingInfoParts/BuildingShelfDetailPart.lua
+++ Assets/CityGame/Lua/View/BuildingInfoParts/BuildingShelfDetailPart.lua
@@ -499,16 +499,16 @@ end
 function BuildingShelfDetailPart:getShelfItemIdCount(itemId,producerId,callback)
     if itemId ~= nil then
         local nowCount = 0
-        if not self.m_data.shelf.good or next(self.m_data.shelf.good) == nil then
+        if not self.m_data.store.locked or next(self.m_data.store.locked) == nil then
             nowCount = 0
         else
-            for key,value in pairs(self.m_data.shelf.good) do
+            for key,value in pairs(self.m_data.store.locked) do
                 if producerId == nil then
-                    if value.k.id == itemId then
+                    if value.key.id == itemId then
                         nowCount = value.n
                     end
                 else
-                    if value.k.id == itemId and value.k.producerId == producerId then
+                    if value.key.id == itemId and value.key.producerId == producerId then
                         nowCount = value.n
                     end
                 end

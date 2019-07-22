7c22a5ad70e24f3477efcf98464ebc1df40bf347
fisher <fisher@scry.info>
2019-07-22 17:03:35 +0800

    //

--- Assets/CityGame/Lua/Controller/BuyBoxCtrl.lua
+++ Assets/CityGame/Lua/Controller/BuyBoxCtrl.lua
@@ -29,10 +29,14 @@ function BuyBoxCtrl:Awake(go)
     self.gameObject = go
     self:_getComponent(go)
     self.luaBehaviour = self.gameObject:GetComponent('LuaBehaviour')
-    self.luaBehaviour:AddClick(self.closeBtn.gameObject,self._clickCloseBtn,self)
+    self.luaBehaviour:AddClick(self.bgBtn.gameObject,self._clickCloseBtn,self)
+    --self.luaBehaviour:AddClick(self.closeBtn.gameObject,self._clickCloseBtn,self)
     self.luaBehaviour:AddClick(self.buyBtn.gameObject,self._clickBuyBtn,self)
     self.numberSlider.onValueChanged:AddListener(function()
-        self:SlidingUpdateText()
+        self:UpdateSlidingText()
+    end)
+    self.numberInput.onEndEdit:AddListener(function()
+        self:UpdateInputText()
     end)
 end
 
@@ -47,8 +51,10 @@ function BuyBoxCtrl:Hide()
 end
 -------------------------------------------------------------获取组件-------------------------------------------------------------------------------
 function BuyBoxCtrl:_getComponent(go)
+    --bgBtn
+    self.bgBtn = go.transform:Find("bgBtn")
     --top
-    self.closeBtn = go.transform:Find("contentRoot/top/closeBtn")
+    --self.closeBtn = go.transform:Find("contentRoot/top/closeBtn")
     self.topName = go.transform:Find("contentRoot/top/topName"):GetComponent("Text")
     --content
     self.iconbg = go.transform:Find("contentRoot/content/goodsInfo/iconbg")
@@ -59,22 +65,23 @@ function BuyBoxCtrl:_getComponent(go)
     self.scoreBg = go.transform:Find("contentRoot/content/goodsInfo/scoreBg")
     self.brandName = go.transform:Find("contentRoot/content/goodsInfo/scoreBg/brandBg/brandName"):GetComponent("Text")
     self.brandNameText = go.transform:Find("contentRoot/content/goodsInfo/scoreBg/brandBg/brandName/brandNameText"):GetComponent("Text")
-    self.popularityText = go.transform:Find("contentRoot/content/goodsInfo/scoreBg/popularity/popularity"):GetComponent("Text")
+    --self.popularityText = go.transform:Find("contentRoot/content/goodsInfo/scoreBg/popularity/popularity"):GetComponent("Text")
     self.popularityValue = go.transform:Find("contentRoot/content/goodsInfo/scoreBg/popularity/popularity/popularityValue"):GetComponent("Text")
-    self.qualityText = go.transform:Find("contentRoot/content/goodsInfo/scoreBg/quality/quality"):GetComponent("Text")
-    self.qualityValue = go.transform:Find("contentRoot/content/goodsInfo/scoreBg/quality/quality/qualityValue"):GetComponent("Text")
+    --self.qualityText = go.transform:Find("contentRoot/content/goodsInfo/scoreBg/quality/quality"):GetComponent("Text")
+    self.qualityValue = go.transform:Find("contentRoot/content/goodsInfo/scoreBg/quality/qualityValue"):GetComponent("Text")
     self.levelBg = go.transform:Find("contentRoot/content/goodsInfo/levelBg")
     self.levelImg = go.transform:Find("contentRoot/content/goodsInfo/levelBg/levelImg"):GetComponent("Image")
-    self.levelText = go.transform:Find("contentRoot/content/goodsInfo/levelBg/levelImg/level"):GetComponent("Text")
-    self.levelValue = go.transform:Find("contentRoot/content/goodsInfo/levelBg/levelImg/level/levelText"):GetComponent("Text")
+    --self.levelText = go.transform:Find("contentRoot/content/goodsInfo/levelBg/levelImg/level"):GetComponent("Text")
+    self.levelValue = go.transform:Find("contentRoot/content/goodsInfo/levelBg/levelImg/levelText"):GetComponent("Text")
 
     self.number = go.transform:Find("contentRoot/content/goodsInfo/number")
     self.shelf = go.transform:Find("contentRoot/content/goodsInfo/number/shelfNumber")
-    self.numberTipText = go.transform:Find("contentRoot/content/goodsInfo/number/shelfNumber/numberTipText"):GetComponent("Text")
+    --self.numberTipText = go.transform:Find("contentRoot/content/goodsInfo/number/shelfNumber/numberTipText"):GetComponent("Text")
     self.shelfNumberText = go.transform:Find("contentRoot/content/goodsInfo/number/shelfNumber/shelfNumberText"):GetComponent("Text")
-    self.tipText = go.transform:Find("contentRoot/content/tipText"):GetComponent("Text")
+    --self.tipText = go.transform:Find("contentRoot/content/tipText"):GetComponent("Text")
+    self.numberInput = go.transform:Find("contentRoot/content/numberInput"):GetComponent("InputField")
     self.numberSlider = go.transform:Find("contentRoot/content/numberSlider"):GetComponent("Slider")
-    self.numberText = go.transform:Find("contentRoot/content/numberSlider/HandleSlideArea/Handle/numberBg/numberText"):GetComponent("Text")
+    --self.numberText = go.transform:Find("contentRoot/content/numberSlider/HandleSlideArea/Handle/numberBg/numberText"):GetComponent("Text")
     --bottom
     self.buyBtn = go.transform:Find("contentRoot/bottom/buyBtn")
     self.buyText = go.transform:Find("contentRoot/bottom/buyBtn/text"):GetComponent("Text")
@@ -108,7 +115,7 @@ function BuyBoxCtrl:initializeUiInfoData()
     self.priceText.text = GetClientPriceString(self.m_data.dataInfo.price)
     self.numberSlider.maxValue = self.m_data.dataInfo.n
     self.numberSlider.value = 1
-    self.numberText.text = "×"..self.numberSlider.value
+    --self.numberText.text = "×"..self.numberSlider.value
     local function callback1(number)
         self.shelfNumberText.text = "×"..number
     end
@@ -117,17 +124,22 @@ end
 --设置多语言
 function BuyBoxCtrl:_language()
     self.topName.text = GetLanguage(28040035)
-    self.tipText.text = GetLanguage(25070001)
+    --self.tipText.text = GetLanguage(25070001)
     self.buyText.text = GetLanguage(25070014)
     self.brandName.text = GetLanguage(25020040)
-    self.popularityText.text = GetLanguage(25020006)
-    self.qualityText.text = GetLanguage(25020005)
-    self.levelText.text = GetLanguage(25020007)
-    self.numberTipText.text = GetLanguage(25020037)
+    --self.popularityText.text = GetLanguage(25020006)
+    --self.qualityText.text = GetLanguage(25020005)
+    --self.levelText.text = GetLanguage(25020007)
+    --self.numberTipText.text = GetLanguage(25020037)
 end
---滑动更新文本
-function BuyBoxCtrl:SlidingUpdateText()
-    self.numberText.text = "×"..self.numberSlider.value
+--滑动更新输入框文本
+function BuyBoxCtrl:UpdateSlidingText()
+    --self.numberText.text = "×"..self.numberSlider.value
+    self.numberInput.text = tostring(self.numberSlider.value)
+end
+--输入框更新滑动条
+function BuyBoxCtrl:UpdateInputText()
+    self.numberSlider.value = ToNumber(self.numberInput.text)
 end
 ---------------------------------------------------------------点击函数-------------------------------------------------------------------------------------
 --关闭

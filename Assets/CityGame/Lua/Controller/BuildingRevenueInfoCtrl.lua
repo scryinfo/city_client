82000d0399319634ddfb1c1f3d012bae730fded2
fisher <fisher@scry.info>
2019-07-24 18:21:21 +0800

    //建筑经营信息通用组件

--- Assets/CityGame/Lua/Controller/BuildingRevenueInfoCtrl.lua
+++ Assets/CityGame/Lua/Controller/BuildingRevenueInfoCtrl.lua
@@ -4,3 +4,144 @@
 --- DateTime: 2019/7/24 14:39
 ---建筑经营详情
 
+BuildingRevenueInfoCtrl = class('BuildingRevenueInfoCtrl',UIPanel)
+UIPanel:ResgisterOpen(BuildingRevenueInfoCtrl)
+
+local indexs   --用来记录经营界面当前折线图索引
+local isbool  --用来记录经营界面当前是否有折线图处于打开状态
+function BuildingRevenueInfoCtrl:initialize()
+    UIPanel.initialize(self,UIType.Normal,UIMode.HideOther,UICollider.None);
+end
+
+function BuildingRevenueInfoCtrl:bundleName()
+    return "Assets/CityGame/Resources/View/BuildingRevenueInfoPanel.prefab"
+end
+
+function BuildingRevenueInfoCtrl:OnCreate(obj)
+    UIPanel.OnCreate(self,obj)
+end
+
+function BuildingRevenueInfoCtrl:Awake(go)
+    self.gameObject = go
+    self:_getComponent(go)
+    self.luaBehaviour = self.gameObject:GetComponent('LuaBehaviour')
+    self.luaBehaviour:AddClick(self.closeBtn.gameObject,self._clickCloseBtn,self)
+end
+
+function BuildingRevenueInfoCtrl:Active()
+    UIPanel.Active(self)
+    indexs = nil
+    isbool = false
+    --实例表
+    self.itemPrefabTab = {}
+    self.buildingType = BuildingType.MaterialFactory    --后边传数据进来要删除
+    self:language()
+    Event.AddListener("calculateLinePanel",self.calculateLinePanel,self)
+end
+
+function BuildingRevenueInfoCtrl:Refresh()
+    self:initializeUiBuildingInfo()
+end
+
+function BuildingRevenueInfoCtrl:Hide()
+    UIPanel.Hide(self)
+end
+-------------------------------------------------------------获取组件---------------------------------------------------------------------------------
+function BuildingRevenueInfoCtrl:_getComponent(go)
+    ---TopRoot
+    --top
+    self.closeBtn = go.transform:Find("topRoot/top/closeBtn")
+    self.topName = go.transform:Find("topRoot/top/topName"):GetComponent("Text")
+    --topType
+    self.goodsTypeText = go.transform:Find("topRoot/topType/goodsType/Text"):GetComponent("Text")
+    self.todaySalesText = go.transform:Find("topRoot/topType/todaySales/Text"):GetComponent("Text")
+    self.proportionText = go.transform:Find("topRoot/topType/proportion/Text"):GetComponent("Text")
+    ---content
+    --Content
+    self.Content = go.transform:Find("content/ScrollView/Viewport/Content")
+    self.linePanel = go.transform:Find("content/ScrollView/Viewport/Content/linePanel")
+    self.itemPrefabBtn = go.transform:Find("content/ScrollView/Viewport/Content/itemPrefabBtn").gameObject
+end
+---------------------------------------------------------------初始化函数------------------------------------------------------------------------------
+--初始化UI信息
+function BuildingRevenueInfoCtrl:initializeUiBuildingInfo()
+    --模拟数据--
+    local datas = {}
+    --随机数种子
+    math.randomseed(os.time())
+    for i = 1, 10 do
+        local data = {}
+        data.name = "小麦"..i
+        data.todaySales = math.random(500,20000)
+        data.proportion = math.random(1,3000)
+        table.insert(datas,data)
+    end
+    for key,value in pairs(datas) do
+        local obj = self:loadingItemPrefab(self.itemPrefabBtn,self.Content)
+        local itemPrefab = itemPrefabBtn:new(value,obj,self.luaBehaviour,key)
+        table.insert(self.itemPrefabTab,itemPrefab)
+    end
+end
+--多语言
+function BuildingRevenueInfoCtrl:language()
+    --根据建筑显示是原料还是商品还是建筑名字（研究所）
+    if self.buildingType == BuildingType.MaterialFactory then
+        self.goodsTypeText.text = "原料"
+    elseif self.buildingType == BuildingType.ProcessingFactory then
+        self.goodsTypeText.text = "商品"
+    elseif self.buildingType == BuildingType.RetailShop then
+        self.goodsTypeText.text = "商品"
+    elseif self.buildingType == BuildingType.Municipal then
+        self.goodsTypeText.text = "数据类型"
+    elseif self.buildingType == BuildingType.Laboratory then
+        self.goodsTypeText.text = "科技资料"
+    end
+    self.todaySalesText.text = "今日销售额"
+    self.proportionText.text = "占昨日销售额比例"
+end
+-----------------------------------------------------------------------------点击函数-------------------------------------------------------------------------
+--关闭
+function BuildingRevenueInfoCtrl:_clickCloseBtn()
+    PlayMusEff(1002)
+    UIPanel.ClosePage()
+end
+
+-----------------------------------------------------------------------------事件函数-------------------------------------------------------------------------
+--计算位置
+function BuildingRevenueInfoCtrl:calculateLinePanel(index)
+    self:openLinePanel(index)
+    if isbool == false then
+        return
+    end
+    if index == 1 then
+        self.Content.transform.localPosition = Vector3(0,0,0)
+    else
+        self.Content.transform:GetComponent("RectTransform").anchoredPosition = Vector2.New(0, 225 * (index - 1)) --.localPosition = Vector3(0, 220 * (index - 1),0)
+    end
+end
+-------------------------------------------------------------------------------------------------------------------------------------------------------------
+--打开折线图
+function BuildingRevenueInfoCtrl:openLinePanel(index)
+    if indexs == nil and isbool == false then
+        indexs = index
+        self.linePanel.transform:SetSiblingIndex(index + 1)
+        self.linePanel.gameObject:SetActive(true)
+        isbool = true
+    elseif indexs == index and isbool == true then
+        indexs = nil
+        self.linePanel.gameObject:SetActive(false)
+        isbool = false
+    elseif indexs ~= index and isbool == true then
+        indexs = index
+        self.linePanel.transform:SetSiblingIndex(index + 1)
+    end
+end
+--加载实例化Prefab
+function BuildingRevenueInfoCtrl:loadingItemPrefab(itemPrefab,itemRoot)
+    local obj = UnityEngine.GameObject.Instantiate(itemPrefab)
+    local objRect = obj.transform:GetComponent("RectTransform")
+    obj.transform:SetParent(itemRoot.transform)
+    objRect.transform.localScale = Vector3.one
+    obj:SetActive(true)
+    return obj
+end
--- Assets/CityGame/Lua/View/BuildingInfo/itemPrefabBtn.lua
+++ Assets/CityGame/Lua/View/BuildingInfo/itemPrefabBtn.lua
@@ -3,17 +3,31 @@
 --- Created by Fisher.
 --- DateTime: 2019/7/24 16:04
 ---建筑经营详情Item
-WarehouseItem = class('WarehouseItem')
+itemPrefabBtn = class('itemPrefabBtn')
 
-function WarehouseItem:initialize(dataInfo,prefab,luaBehaviour,keyId,buildingType,stateType)
+function itemPrefabBtn:initialize(data,prefab,luaBehaviour,keyId)
+    self.data = data
+    self.keyId = keyId
+
+    self.bgBtn = prefab.transform:Find("bgBtn")
+    self.iconImg = prefab.transform:Find("info/icon/iconImg"):GetComponent("Image")
+    self.nameText = prefab.transform:Find("info/nameText"):GetComponent("Text")
+    self.brandName = prefab.transform:Find("info/brandName"):GetComponent("Text")
+    self.todaySalesText = prefab.transform:Find("todaySales/todaySalesText"):GetComponent("Text")
+    self.proportionText = prefab.transform:Find("proportion/proportionText"):GetComponent("Text")
+
+    luaBehaviour:AddClick(self.bgBtn.gameObject,self._clickBgBtn,self)
     self:InitializeData()
 end
 
-function WarehouseItem:InitializeData()
-
+function itemPrefabBtn:InitializeData()
+    self.iconImg.sprite = SpriteManager.GetSpriteByPool(2101001)
+    self.nameText.text = self.data.name
+    self.brandName.text = ""
+    self.todaySalesText.text = self.data.todaySales
+    self.proportionText.text = self.data.proportion.."%"
 end
 
---删除后刷新ID
-function WarehouseItem:RefreshID(id)
-    self.keyId = id;
+function itemPrefabBtn:_clickBgBtn(ins)
+    Event.Brocast("calculateLinePanel",ins.keyId)
 end

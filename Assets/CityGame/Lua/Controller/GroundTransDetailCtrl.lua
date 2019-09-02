3bc87c57d50f766c3f58881d8a5f62c2176df097
fisher <fisher@scry.info>
2019-09-02 09:54:28 +0800

    //多语言

--- Assets/CityGame/Lua/Controller/GroundTransDetailCtrl.lua
+++ Assets/CityGame/Lua/Controller/GroundTransDetailCtrl.lua
@@ -50,7 +50,7 @@ function GroundTransDetailCtrl:Active()
     GroundTransDetailPanel.sellText06.text = GetLanguage(22010004)
     GroundTransDetailPanel.sellingText07.text = GetLanguage(22010006)
     GroundTransDetailPanel.rentingText08.text = GetLanguage(22010005)
-    GroundTransDetailPanel.ownerText.text = "所有者:"
+    GroundTransDetailPanel.ownerText.text = GetLanguage(18020009)..":"
     GroundTransDetailPanel.prosperityText.text = GetLanguage(20170003)
     Event.AddListener("_saveGroundProsperity",self._saveGroundProsperity,self)
     self.blockData = {}
